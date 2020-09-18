f04e41f2f513d6ee38dd414ccabd5f6add2d032c
vyk1 <victoriabotelho14@gmail.com>
2020-09-18 01:42:46 -0300

    Feat: Criando navegação com rotas

---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+const Records = () => {
+    return (
+        <div className="home-container">
+            <h1>iai</h1>
+        </div>
+    );
+}
+
+export default Records;
