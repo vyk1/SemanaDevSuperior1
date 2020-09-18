44513ca76f85e98a563e49428af1cc950c62233e
vyk1 <victoriabotelho14@gmail.com>
2020-09-18 02:29:02 -0300

    Feat: Criando página de listagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { BASE_URL } from '../../utils';
+import { RecordsResponse } from './types';
+import { formatDate } from './helpers';
 import './styles.css'
+import Pagination from './Pagination';
+import { Link } from 'react-router-dom';
 
 const Records = () => {
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>()
+    const [activePage, setActivePage] = useState(0)
+
+    useEffect(() => {
+        Axios.get(`${BASE_URL}/records?linesPerpage=12&page=${activePage}`)
+            .then(res => setRecordsResponse(res.data))
+    }, [activePage]);
     return (
-        <div className="home-container">
-            <h1>iai</h1>
+        <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">VER GRÁFICOS</button>
+                </Link>
+            </div>
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* Optional Chainning */}
+                    {
+                        recordsResponse?.content.map(r => (
+                            <tr key={r.id}>
+                                <td>{formatDate(r.moment)}</td>
+                                <td>{r.name}</td>
+                                <td>{r.age}</td>
+                                <td className="text-secondary">{r.gamePlatform}</td>
+                                <td>{r.genreName}</td>
+                                <td className="text-primary">{r.gameTitle}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            <Pagination
+                activePage={activePage}
+                goToPage={(index: number) => setActivePage(index)}
+                totalPages={recordsResponse?.totalPages}
+            />
         </div>
     );
 }
